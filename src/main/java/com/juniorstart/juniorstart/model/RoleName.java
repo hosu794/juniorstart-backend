1c47f04ddc9677b4bc49bf9b275e5ddd38be2268
Grzegorz Szczesny <grzeszczesny14@gmail.com>
2020-08-30 10:47:40 +0200

    Create User Model with a roles and auditing.

---
+++ src/main/java/com/juniorstart/juniorstart/model/RoleName.java
@@ -0,0 +1,6 @@
+package com.juniorstart.juniorstart.model;
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/juniorstart/juniorstart/model/User.java
@@ -0,0 +1,59 @@
+package com.juniorstart.juniorstart.model;
+
+import com.juniorstart.juniorstart.model.audit.DateAudit;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "username"
+        }),
+        @UniqueConstraint(columnNames = {
+                "email"
+        })
+})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends DateAudit {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 40)
+    private String name;
+
+    @NotBlank
+    @Size(max = 15)
+    private String username;
+
+    @NaturalId
+    @NotBlank
+    @Size(max = 40)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 100)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+}
