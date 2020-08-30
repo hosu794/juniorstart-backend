6199c2f251d1721b121e9c35d6667cecc6b9070c
Grzegorz Szczesny <grzeszczesny14@gmail.com>
2020-08-30 19:36:58 +0200

    Create payload direction and RegisterRequest object for handle register request .

---
+++ src/main/java/com/juniorstart/juniorstart/payload/RegisterRequest.java
@@ -0,0 +1,17 @@
+package com.juniorstart.juniorstart.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    private String username;
+    private String name;
+    private String email;
+    private String password;
+
+}
