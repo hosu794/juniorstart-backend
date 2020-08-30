a484a8b85286046241e94570c8708151372be3cf
Grzegorz Szczesny <grzeszczesny14@gmail.com>
2020-08-30 08:23:33 +0200

    First Commit.

---
+++ src/main/java/com/juniorstart/juniorstart/JuniorstartApplication.java
@@ -0,0 +1,13 @@
+package com.juniorstart.juniorstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorstartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juniorstart/juniorstart/JuniorstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juniorstart.juniorstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniorstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
