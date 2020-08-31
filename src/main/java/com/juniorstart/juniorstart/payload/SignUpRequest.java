6cdb5bc0e877c0c89ddae15c10cd251a2474bcf6
Grzegorz Szczesny <grzeszczesny14@gmail.com>
2020-08-31 15:21:09 +0200

    Finish feature without a fully documented and tests.

---
+++ src/main/java/com/juniorstart/juniorstart/payload/SignUpRequest.java
@@ -0,0 +1,22 @@
+package com.juniorstart.juniorstart.payload;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class SignUpRequest {
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+}
---
+++ src/main/java/com/juniorstart/juniorstart/security/CustomUserDetailsService.java
@@ -0,0 +1,45 @@
+package com.juniorstart.juniorstart.security;
+
+
+
+import com.juniorstart.juniorstart.exception.ResourceNotFoundException;
+import com.juniorstart.juniorstart.model.User;
+import com.juniorstart.juniorstart.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by rajeevkumarsingh on 02/08/17.
+ */
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email : " + email)
+        );
+
+        return UserPrincipal.create(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+            () -> new ResourceNotFoundException("User", "id", id)
+        );
+
+        return UserPrincipal.create(user);
+    }
+}
