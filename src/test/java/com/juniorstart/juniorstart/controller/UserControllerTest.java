f858666992679a28bd16e7df02c462b612ca9c83
Ragnar <wit.dawid@interia.pl>
2020-09-19 18:40:52 +0200

    In progress

--- src/test/java/com/juniorstart/juniorstart/controller/UserControllerTest.java
+++ src/test/java/com/juniorstart/juniorstart/controller/UserControllerTest.java
@@ -1,5 +1,9 @@
 package com.juniorstart.juniorstart.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.juniorstart.juniorstart.payload.ApiResponse;
 import com.juniorstart.juniorstart.payload.ChangeMailRequest;
 import com.juniorstart.juniorstart.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,20 +12,31 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import java.util.UUID;
+
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class UserControllerTest {
     @Mock
     UserService userService;
-
-    @Mock
-    ChangeMailRequest changeMailRequest;
 
     @InjectMocks
     UserController userController;
@@ -35,5 +50,24 @@ class UserControllerTest {
 
     @Test
     void name() throws Exception {
+        //Given
+        ResponseEntity<ApiResponse> response = ResponseEntity.ok((new ApiResponse(true,"Test Ok")));
+        ChangeMailRequest request = new ChangeMailRequest("test@test.com",UUID.randomUUID(),"Password");
+        given(userService.changeEmail(any(ChangeMailRequest.class))).willReturn(response);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson=ow.writeValueAsString(request );
+        System.out.println(requestJson);
+
+        MvcResult result = mockMvc.perform(post("/changeMail")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestJson))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        System.out.println(result.getResponse().getContentAsString());
     }
 }
