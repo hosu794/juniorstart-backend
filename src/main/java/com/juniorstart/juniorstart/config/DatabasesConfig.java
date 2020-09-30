6e8fcb459ff17c58bb05596bddea66039168106f
adrian <adrian.stypinski@gmail.com>
2020-09-30 16:42:41 +0200

    B037 | Added logger database to DatabasesConfig

--- src/main/java/com/juniorstart/juniorstart/config/DatabasesConfig.java
+++ src/main/java/com/juniorstart/juniorstart/config/DatabasesConfig.java
@@ -36,4 +36,20 @@ public class DatabasesConfig {
                 .type(HikariDataSource.class)
                 .build();
     }
+
+    // Log database
+    @Bean
+    @ConfigurationProperties("logs.datasource")
+    public DataSourceProperties logsDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("logs.datasource")
+    public DataSource logsDataSource() {
+        return entitiesDataSourceProperties()
+                .initializeDataSourceBuilder()
+                .type(HikariDataSource.class)
+                .build();
+    }
 }
