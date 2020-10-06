7b137475c9fe3ade6ecfb80529cdce8d7c810bce
Adrian <adrian.stypinski@gmail.com>
2020-10-06 23:21:39 +0200

    B043 | Added H2 database for testing

--- src/test/java/com/juniorstart/juniorstart/email/MailTemplateCreatorTest.java
+++ src/test/java/com/juniorstart/juniorstart/email/MailTemplateCreatorTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -16,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class MailTemplateCreatorTest {
 
     @Autowired
--- src/test/java/com/juniorstart/juniorstart/service/JobOfferServiceTest.java
+++ src/test/java/com/juniorstart/juniorstart/service/JobOfferServiceTest.java
@@ -11,6 +11,8 @@ import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import com.juniorstart.juniorstart.model.AuthProvider;
@@ -20,6 +22,7 @@ import com.juniorstart.juniorstart.repository.JobOfferRepository;
 import com.juniorstart.juniorstart.repository.UserDao;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @RunWith(MockitoJUnitRunner.class)
 public class JobOfferServiceTest {
 	UserDao userDao = Mockito.mock(UserDao.class);
